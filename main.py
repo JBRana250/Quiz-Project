f2b60bd166928299155a9c5bc1c392955473842c
JB Rana <101071450+JBRana250@users.noreply.github.com>
2022-03-21 21:06:07 +0000

    Fixed issue with printing text, added rest of component

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+# Component 1
 while True:
-  name_check = input("Do you want to input a username     or not? Please type 'y' for yes and 'n' for no: ")
+  name_check = input("Do you want to input a username or not? Input 'y' for yes and 'n' for no: ")
   if name_check == "y":
-    print("ok")
+    username = input("What do you want me to call you?: ")
+    print("Hello, {}!".format(username))
     break
   elif name_check == "n":
-    print("alr")
     break
   else:
     print("Input is invalid, please try again.")
+if name_check == "y":
+  print("Welcome to my quiz, {}! This quiz will test your python programming skills.".format(username))
+elif name_check == "n":
+  print("Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!") 
+  # will insert length of quiz later
+  
