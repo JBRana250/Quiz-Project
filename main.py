c41f08c0f1eb50e8bdbb7bc18dfe97005119380f
JB Rana <101071450+JBRana250@users.noreply.github.com>
2022-03-21 21:50:51 +0000

    made it so that username can only be inputted in alphabets

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # Component 1
 while True:
-  name_check = input("Do you want to input a username or not? Input 'y' for yes and 'n' for no: ")
+  name_check = input("Do you want to input a username or not? Input 'y' for yes and  'n' for no: ")
   if name_check == "y":
-    username = input("What do you want me to call you?: ")
-    print("Hello, {}!".format(username))
-    break
+    username = input("What do you want me to call you? Please input username in      letters only: ")
+    if username.isalpha():
+      print("Hello, {}!".format(username))
+      break
+    else:
+      print("Please input username in letters only.")
   elif name_check == "n":
     break
   else:
     print("Input is invalid, please try again.")
 if name_check == "y":
-  print("Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!") 
+  print("Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good  luck!") 
 elif name_check == "n":
-  print("Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!") 
+  print("Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good  luck!") 
   # will insert length of quiz later
   
