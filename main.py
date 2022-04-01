c5f25b2a310a4adff06b495abf0a63d3050241ef
JB Rana <101071450+JBRana250@users.noreply.github.com>
2022-04-01 00:09:13 +0000

    fixed issue with input(), made long input command that fixed issue where text printed wrong.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Global functions / variables / collections
+def long_input(text):
+  print(text, end='')
+  return input()
 # Component 1 ********************
 def QuizIntro():
     def get_user():  #gets username if they want
@@ -43,47 +47,47 @@ def QuizIntro():
 
     name_check_print(0)
     # will insert length of quiz later
-
-
-# ******************************
-#Quiz_Intro()
+  
 # Components 2 and 3 ***********
-correct_answers = 0
-qone_answers = ["for", "while"]
-qtwo_answers = ["input", "print"]
-qthree_answers = ["=", "=="]
-qfour_answers = ["{}", "[]", "()"]
-qfive_answers = ["while", "for"]
-qsix_answers = ["try", "except"]
-qseven_answers = ["print", "input"]
-qeight_answers = ["*", "x"]
-qnine_answers = ["[]", "{}", "()"]
-qten_answers = ["value", "syntax"]
-Questions = {"What type of loop iterates over a sequence?: ":qone_answers, "What function is used to get an input from the user?: ":qtwo_answers, "What symbol is used to assign a variable?: ":qthree_answers, "What brackets indicate a dictionary? Type the brackets directly into the input: ":qfour_answers, "Which type of loop repeats code as long as the condition is true?: ":qfive_answers, "What syntax is often used in exception handling, to try to run a piece of code?":qsix_answers, "What function is used to print text out into the console?: ":qseven_answers, "What symbol is used for multiplication in python?: ":qeight_answers, "What brackets indicate a list? Type the brackets directly into the input: ":qnine_answers, "What error occurs when the user inputs an invalid value type for a function?: ":qten_answers}
-
-length_of_quiz = len(Questions)
-question_num = 1
-for Question in Questions:
-  user_answer = input("Question {}: {}".format(question_num, Question))
-  user_answer = user_answer.strip().lower()
-  answers_list = Questions[Question]
-  correct_answer = answers_list[0]
-  answers_list.pop(0)
-  def check_wrong_answers():
-    for answer in answers_list:
-      if answer in user_answer:
-        user_wrong = 1
+def StartQuestions():
+  correct_answers = 0
+  qone_answers = ["for", "while"]
+  qtwo_answers = ["input", "print"]
+  qthree_answers = ["=", "=="]
+  qfour_answers = ["{}", "[]", "()"]
+  qfive_answers = ["while", "for"]
+  qsix_answers = ["try", "except"]
+  qseven_answers = ["print", "input"]
+  qeight_answers = ["*", "x"]
+  qnine_answers = ["[]", "{}", "()"]
+  qten_answers = ["value", "syntax"]
+  Questions = {"What type of loop iterates over a sequence?: ":qone_answers, "What function is used to get an input from the user?: ":qtwo_answers, "What symbol is used to assign a variable?: ":qthree_answers, "What brackets indicate a dictionary? Type the brackets directly into the input: ":qfour_answers, "Which type of loop repeats code as long as the condition is true?: ":qfive_answers, "What syntax is often used in exception handling, to try to run a piece of code?":qsix_answers, "What function is used to print text out into the console?: ":qseven_answers, "What symbol is used for multiplication in python?: ":qeight_answers, "What brackets indicate a list? Type the brackets directly into the input: ":qnine_answers, "What error occurs when the user inputs an invalid value type for a function?: ":qten_answers}
+  length_of_quiz = len(Questions)
+  question_num = 1
+  for Question in Questions:
+    user_answer = long_input("Question {}: {}".format(question_num, Question))
+    user_answer = user_answer.strip().lower()
+    answers_list = Questions[Question]
+    correct_answer = answers_list[0]
+    answers_list.pop(0)
+    def check_wrong_answers():
+      for answer in answers_list:
+        if answer in user_answer:
+          user_wrong = 1
+          return user_wrong, answer
+          break
+        user_wrong = 0
+        answer = None
         return user_wrong, answer
-        break
-      user_wrong = 0
-      answer = None
-      return user_wrong, answer
-  user_wrong = check_wrong_answers()
-  if user_wrong[0] == 1:
-    print("You got that wrong, you included a wrong answer in your answer which was '{}'.".format(user_wrong[1]))
-  elif correct_answer in user_answer:
-    print("Correct!")
-    correct_answers = correct_answers + 1
-  elif user_wrong[0] == 0:
-    print("You got that wrong, the correct answer was {}.".format(correct_answer))
-  question_num = question_num + 1
+    user_wrong = check_wrong_answers()
+    if user_wrong[0] == 1:
+      print("You got that wrong, you included a wrong answer in your answer which was '{}'.".format(user_wrong[1]))
+    elif correct_answer in user_answer:
+      print("Correct!")
+      correct_answers = correct_answers + 1
+    elif user_wrong[0] == 0:
+      print("You got that wrong, the correct answer was {}.".format(correct_answer))
+    question_num = question_num + 1
+#RUN*PROGRAM******************************
+QuizIntro()
+StartQuestions()
