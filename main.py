be2b550ff9bf9f2729b217af57f28cdd55fb381f
JB Rana <101071450+JBRana250@users.noreply.github.com>
2022-04-04 22:49:31 +0000

    Made it so that txt vers is now global

--- main.py
+++ main.py
@@ -18,8 +18,26 @@ qnine_answers = ["[]", "{}", "()"]
 qten_answers = ["value", "syntax"]
 
 #questions collection, needed in global since length of quiz is needed as global.
-Questions = {"What type of loop iterates over a sequence?: ":qone_answers, "What function is used to get an input from the user?: ":qtwo_answers, "What symbol is used to assign a variable?: ":qthree_answers, "What brackets indicate a dictionary? Type the brackets directly into the input: ":qfour_answers, "Which type of loop repeats code as long as the condition is true?: ":qfive_answers, "What syntax is often used in exception handling, to try to run a piece of code?":qsix_answers, "What function is used to print text out into the console?: ":qseven_answers, "What symbol is used for multiplication in python?: ":qeight_answers, "What brackets indicate a list? Type the brackets directly into the input: ":qnine_answers, "What error occurs when the user inputs an invalid value type for a function?: ":qten_answers}
+Questions = {"What type of loop iterates over a sequence?: ":qone_answers, "What function is used to get an input from the user?: ":qtwo_answers, "What symbol is used to assign a variable?: ":qthree_answers, "What brackets indicate a dictionary? Type the brackets directly into the input: ":qfour_answers, "Which type of loop repeats code as long as the condition is true?: ":qfive_answers, "What syntax is often used in exception handling, to try to run a piece of code?: ":qsix_answers, "What function is used to print text out into the console?: ":qseven_answers, "What symbol is used for multiplication in python?: ":qeight_answers, "What brackets indicate a list? Type the brackets directly into the input: ":qnine_answers, "What error occurs when the user inputs an invalid value type for a function?: ":qten_answers}
 length_of_quiz = len(Questions)
+
+username = None
+
+txtvers_indexzero_n = "Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good  luck!".format(length_of_quiz)
+txtvers_indexzero_y = "Welcome to my quiz, {}! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!".format(username, length_of_quiz)
+
+# for when I need to update the username
+def format_text_versions(length_of_quiz, username):
+  txtvers_indexzero_n = "Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good  luck!".format(length_of_quiz)
+  txtvers_indexzero_y = "Welcome to my quiz, {}! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!".format(username, length_of_quiz)
+  text_versions = {txtvers_indexzero_n: txtvers_indexzero_y}
+  return text_versions
+
+#dictionary storing different versions of text depending on if user input a username or not. I used a dictionary in case I need to add more text versions.
+
+text_versions = {txtvers_indexzero_n: txtvers_indexzero_y}
+
+keys_list_txtvers = list(text_versions)
 
 # Component 1 ********************
 
@@ -54,22 +72,16 @@ def quiz_intro():
     #getting username and name_check from get_user()
     username_and_name_check = get_user()
     username = username_and_name_check[0]
-    name_check = username_and_name_check[1]  
+    name_check = username_and_name_check[1]   
 
-    txtvers_indexzero_n = "Welcome to my quiz! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good  luck!".format(length_of_quiz)
-    txtvers_indexzero_y = "Welcome to my quiz, {}! This quiz will test your python programming skills. There are a total of {} questions in this quiz. Good luck!".format(username, length_of_quiz)
-
-    #dictionary storing different versions of text depending on if user input a username or not. I used a dictionary in case I need to add more text versions.
-    text_versions = {txtvers_indexzero_n: txtvers_indexzero_y} 
+    updated_text_versions = format_text_versions(length_of_quiz, username)
   
-    keys_list_txtvers = list(text_versions)
-
     #prints text version depending on if user wanted username or not
     def name_check_print(index):
         if name_check == "n":
             print(keys_list_txtvers[index])
         else:                                    
-            print(text_versions.get(keys_list_txtvers[index]))
+            print(updated_text_versions.get(keys_list_txtvers[index]))
           
     name_check_print(0)
     print("")
@@ -112,9 +124,15 @@ def start_questions():
       print("You got that wrong, the correct answer was {}.".format(correct_answer))
       print("")
     question_num = question_num + 1
+  return(correct_answers)
 
+# Component 4 *****************
+def print_correct_answers(correct_answers):
+    print("You got {} answers right.".format(correct_answers))
 #RUN*PROGRAM******************************
-
 def run_quiz():
   quiz_intro()
   correct_answers = start_questions() #get num of correct ans
+  print_correct_answers(correct_answers)
+
+run_quiz()
