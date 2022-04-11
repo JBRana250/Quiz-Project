303752b571ad1d74c100cb895bda07fc3c4a1468
JB Rana <101071450+JBRana250@users.noreply.github.com>
2022-04-11 00:21:24 +0000

    final vers: added yes/no variations to make program more flexible

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def format_text_versions(length_of_quiz, username, correct_answers):
   return text_versions
 
 # this function prints out a version of text depending on if the user wanted a username or not, using an updated_text_versions dictionary.
-def name_check_print(name_check, keys_list_txtvers, updated_text_versions, index):
-  if name_check == "n":                        
+def name_check_print(yes_no_check, keys_list_txtvers, updated_text_versions, index):
+  if yes_no_check == "no":                        
     print(keys_list_txtvers[index])
   else:                                                print(updated_text_versions.get(keys_list_txtvers[index]))
 
@@ -45,34 +45,72 @@ def check_wrong_answers(answers_list, user_answer):
   user_wrong = 0
   answer = None
   return user_wrong, answer
-  
+
+yes_variations = ["yes", "ye", "ok", "okay", "sure", "yup", "yep", "yeah", "yea"]
+no_variations = ["no", "nah", "no thanks", "nope"]
+
+def check_yes_variations(variable):
+  for yes in yes_variations:
+    if yes in variable:
+      return 1
+    elif variable == "y":
+      return 1
+    else:
+      pass
+  return 0
+
+def check_no_variations(variable):
+  for no in no_variations:
+    if no in variable:
+      return 1
+    elif variable == "n":
+      return 1
+    else:
+      pass
+  return 0
+
+def check_yes_or_no(variable):
+  yes_check = check_yes_variations(variable)
+  no_check = check_no_variations(variable)
+  if yes_check == 1:
+    if no_check == 1:
+      return "both"
+  if yes_check == 1:
+    return "yes"
+  if no_check == 1:
+    return "no"
+
 # Component 1 ********************
 
 def quiz_intro():
     def get_user():  #gets username if they want
         while True:  
-            name_check = long_input("Do you want to input a username or not? Input 'y' for yes and 'n' for no: ")
+            name_check = long_input("Do you want to input a username or not?: ")
             name_check = name_check.strip().lower()
 
-            #if they want username
-            if name_check == "y":
+            yes_no_check = check_yes_or_no(name_check)
+            
+            if yes_no_check == "yes":
                 print("")
                 while True:
                   username = long_input("What do you want me to call you? Please input username in letters only: ")
                   username = username.strip()
                   if username.isalpha():
                     print("")  
-                    return (username, name_check)
+                    return (username, yes_no_check)
                     
                   else:
                     print("Please input username in letters only.")
                     print("")
 
             #if they dont want username
-            elif name_check == "n":
+            elif yes_no_check == "no":
                 print("")
                 username = None
-                return (username, name_check)
+                return (username, yes_no_check)
+            elif yes_no_check == "both":
+              print("Please make up your mind! (Don't input both 'yes' and 'no'!)")
+              print("")
 
             #if user input is not 'y' or 'n'
             else:
@@ -80,18 +118,18 @@ def quiz_intro():
                 print("")
 
     #getting username and name_check from get_user()
-    username_and_name_check = get_user()
-    username = username_and_name_check[0]
-    name_check = username_and_name_check[1]   
+    username_and_yesno_check = get_user()
+    username = username_and_yesno_check[0]
+    yes_no_check = username_and_yesno_check[1]   
     correct_answers = None
   
     updated_text_versions = format_text_versions(length_of_quiz, username, correct_answers)
     keys_list_txtvers = list(updated_text_versions)
 
           
-    name_check_print(name_check, keys_list_txtvers, updated_text_versions, 0)
+    name_check_print(yes_no_check, keys_list_txtvers, updated_text_versions, 0)
     print("")
-    return name_check, keys_list_txtvers, username
+    return yes_no_check, keys_list_txtvers, username
   
 # Components 2 and 3 **********
 def start_questions():
@@ -110,13 +148,12 @@ def start_questions():
     answers_list.pop(0)
         
     user_wrong = check_wrong_answers(answers_list, user_answer)
-    print(user_wrong)
-    
+  
     if user_wrong[0] == 1: #if user include wrong answer
       print("You got that wrong, you included a wrong answer in your answer which was '{}'.".format(user_wrong[1]))
       print("")
     elif correct_answer in user_answer: #if user include correct ans
-      print("Correct!")
+      print("** Correct! **")
       print("")
       correct_answers = correct_answers + 1
     elif user_wrong[0] == 0: #if user did not include any ans from list
@@ -126,37 +163,42 @@ def start_questions():
   return(correct_answers)
 
 # Component 4 *****************
-def print_correct_answers(name_check, keys_list_txtvers, correct_answers, username):
+def print_correct_answers(yes_no_check, keys_list_txtvers, correct_answers, username):
   updated_text_versions = format_text_versions(length_of_quiz, username, correct_answers)
   keys_list_txtvers = list(updated_text_versions)
   
-  name_check_print(name_check, keys_list_txtvers, updated_text_versions, 1)
+  name_check_print(yes_no_check, keys_list_txtvers, updated_text_versions, 1)
   print("")
 #RUN*PROGRAM******************************
+#puts quiz intro out of loop, so that it doesnt repeat when the user wants to try again and again.
 quiz_intro_variables = quiz_intro()
-name_check = quiz_intro_variables[0]
+yes_no_check = quiz_intro_variables[0]
 keys_list_txtvers = quiz_intro_variables[1]
 username = quiz_intro_variables[2]
-
+#*****************************************
 def run_quiz():
   correct_answers = start_questions()
-  print_correct_answers(name_check, keys_list_txtvers, correct_answers, username)
+  print_correct_answers(yes_no_check, keys_list_txtvers, correct_answers, username)
 
 #Component 5 ***************************
 def loop_run_quiz():
   while True:
     run_quiz()
     while True:
-      rerun_check = input("Do you want to try again? Input 'y' if yes, input 'n' if no: ")
-      if rerun_check == "y":
+      rerun_check = input("Do you want to try again?: ")
+      yes_no_check = check_yes_or_no(rerun_check)
+      if yes_no_check == "yes":
         print("")
         break
-      elif rerun_check == "n":
+      elif yes_no_check == "no":
         break
+      elif yes_no_check == "both":
+        print("Please make up your mind! (Don't input both 'yes' and 'no'!)")
+        print("")
       else:
         print("Input invalid, please try again")
         print("")
-    if rerun_check == "n":
+    if yes_no_check == "no":
       break
 
 loop_run_quiz()
